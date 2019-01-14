e644470ce5606d30b5c58d96ae9d94409b328439
xus <sealoffire@hotmail.com>
2019-01-14 21:34:53 +0800

    创建敌人

---
+++ Assets/Scripts/EnemyManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyManager : MonoBehaviour
+{
+    // public PlayerHealth playerHealth;
+    public GameObject enemy;
+    public float spawnTime = 10f;
+    public Transform[] spawnPoints;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        InvokeRepeating("Spawn", spawnTime, spawnTime);
+    }
+
+    // Update is called once per frame
+    private void Spawn()
+    {
+
+        foreach (Transform spawnPoint in spawnPoints)
+        {
+            Instantiate(enemy, spawnPoint.position, spawnPoint.rotation);
+        }
+    }
+}
