f1cd4fd3048960621222c0328031779d761db85d
xus <sealoffire@hotmail.com>
2019-01-23 21:49:34 +0800

    分批次创建敌人
    分批次创建敌人

--- Assets/Scripts/EnemyManager.cs
+++ Assets/Scripts/EnemyManager.cs
@@ -9,10 +9,66 @@ public class EnemyManager : MonoBehaviour
     public float spawnTime = 10f;
     public Transform[] spawnPoints;
 
+    // 是否已经刷出这波敌人
+    private struct Wave
+    {
+        public string MethodName;
+        public int AppearTime;
+        public Wave(string MethodName, int AppearTime)
+        {
+            this.MethodName = MethodName;
+            this.AppearTime = AppearTime;
+        }
+    }
+
+    private Wave[] waves = { new Wave("FirstWave", 2), new Wave("SecondWave", 5), new Wave("ThirdWave", 10), new Wave("BossWave", 12) };
+
+    //private int index = 0;
+
     // Start is called before the first frame update
     private void Start()
     {
         InvokeRepeating("Spawn", spawnTime, spawnTime);
+
+        foreach (Wave wave in this.waves)
+        {
+            Invoke(wave.MethodName, wave.AppearTime);
+        }
+    }
+
+    /// <summary>
+    /// 创建第1波敌人
+    /// 普通敌人
+    /// </summary>
+    private void FirstWave()
+    {
+        Debug.Log("创建第1波敌人");
+    }
+
+    /// <summary>
+    /// 创建第2波敌人
+    /// pow up掉落
+    /// </summary>
+    private void SecondWave()
+    {
+        Debug.Log("创建第2波敌人");
+    }
+
+    /// <summary>
+    /// 创建第3波敌人
+    /// 普通敌人
+    /// </summary>
+    private void ThirdWave()
+    {
+        Debug.Log("创建第3波敌人");
+    }
+
+    /// <summary>
+    /// 创建boss
+    /// </summary>
+    private void BossWave()
+    {
+        Debug.Log("创建第BOSS");
     }
 
     // Update is called once per frame
