0dc045043a2ab9e3f9ef3ecccdd70773e298db5e
xus <sealoffire@hotmail.com>
2019-02-18 23:12:17 +0800

    阶段性修改
    阶段性修改

---
+++ Assets/Scripts/Colliders/Attacked.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace STG_Demo.PlayerCollied
+{
+    // 被攻击
+    public class Attacked : MonoBehaviour
+    {
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag("Enemy Bullet"))
+            {
+                // TODO 被敌人的子弹击中
+                Debug.Log("被敌人的子弹击中");
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Colliders/Pickup.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace STG_Demo.Colliders
+{
+    // 拾取物品
+    public class Pickup : MonoBehaviour
+    {
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag("Item"))
+            {
+                // TODO 被敌人的子弹击中
+                Debug.Log("拾取物品");
+            }
+        }
+    }
+}
